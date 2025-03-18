84d5cc2770d2d0713bac6d6fb958358b5384cef6
Joel Dixon <dixon.joel03@gmail.com>
2025-03-17 20:46:33 -0500

    Ex.3

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(128))  # Add the password field
+    password = db.Column(db.String(512))  # Add the password field
 
     def __init__(self, first_name, last_name, username, password):
         self.first_name = first_name
